1a13a4d6792087638568915d19fc2d670493ee53
Tadidas <56699457+Tadidas@users.noreply.github.com>
2022-12-24 17:29:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,30 @@ FB.ui(
   },
   response => (console.log(response)) // Callback function
 );
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <style>
+        /*Put the CSS here */
+        .button{
+            background-color: aqua;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>My First Interface !</h1>
+    <button id="firstbutton" class="button">Click on me !</button>
+    <script>
+        /*Put the javascript here */
+        let buttonAlert = document.getElementById("firstbutton");
+        buttonAlert.onclick = function(){
+            alert("you click on me !")
+        };
+
+    </script>
+</body>
+
+</html>
